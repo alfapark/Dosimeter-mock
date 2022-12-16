51fa4fca34c58bfa5db70cdaff8db3468a0e9daa
Jan Pokorný <pokorj54@fit.cvut.cz>
2022-12-16 02:16:01 +0100

    network scanner

---
+++ network_scanner.py
@@ -0,0 +1,37 @@
+import subprocess
+
+
+def parse_one_cell(cell):
+    lines = [line.strip() for line in cell.split('\n')]
+    res = dict()
+    for line in lines:
+        #print(line)
+        if '=' in line and '  ' in line:
+            for part in line.split('  '):
+                key, val = part.split('=', 1)
+                res[key] = val.strip()
+        elif ':' not in line:
+            continue
+        else:
+            key, val = line.split(':', 1)
+            res[key] = val.strip()
+    return res
+
+def parse_interface():
+    process = subprocess.run(['iwlist', 'scan'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    s = process.stdout.decode('utf-8')
+
+    l = s.split('Cell ')[1:] # splitting by cell and then removing the initial info
+
+    l = [s[s.index('A'):] for s in l] # remove cell id and dash
+    l = [parse_one_cell(c) for c in l] # parse it
+    l = {c['Address']:c for c in l} # to dict
+    return l
+
+def get_signal_strength(address):
+    return parse_interface()[address]
+
+if __name__ == '__main__':
+    #interface = 'wlp1s0' # 'wlan0' for rpi
+    print(parse_interface())
