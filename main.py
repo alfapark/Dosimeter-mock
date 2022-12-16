1caab8c519f4eeef073bc56078d1927215ccedea
Jan Pokorný <pokorj54@fit.cvut.cz>
2022-12-16 02:41:26 +0100

    minimal concept

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from bluepy.btle import Scanner
+#import libraries
+import RPi.GPIO as GPIO
+import time
+import network_scanner
+import sys
+
+address = sys.argv[1]
+
+#GPIO Basic initialization
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(True)
+
+led1 = 4
+led2 = 17
+GPIO.setup(led1,GPIO.OUT)
+GPIO.setup(led2,GPIO.OUT)
 
 while True:
-    ble_list = Scanner().scan(3)
-    for dev in ble_list:
-        print("rssi: {} ; mac: {}".format(dev.rssi,dev.addr))
+    
+    strength = -network_scanner.get_signal_strength(address)
+    print(strength)
+    GPIO.output(led1, strength < 30)
+    GPIO.output(led2, strength < 50)
+    time.sleep(1)
